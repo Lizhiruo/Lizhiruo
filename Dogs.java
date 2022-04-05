3b8bbe0a4e08ba3161b8bfa38ee5a6b93db565ac
Lizhiruo <994465209@qq.com>
2022-04-05 18:31:47 +0800

    first commit

---
+++ Dogs.java
@@ -0,0 +1,12 @@
+
+public class Dogs {
+	String yanse;
+	String daxiao;
+	String maofachangduan;
+	String pingzhong;
+		
+	public void dazhaohu(String dog) {
+		System.out.println("看见小明,"+dog+"摇了摇尾巴~"+"\t");
+	}
+}
+
